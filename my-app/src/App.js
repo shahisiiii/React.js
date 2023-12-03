d2e0a3a3da4fd8c2869d81db876b5af9b42ea661
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-12-03 22:30:51 +0530

    add button function created

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,17 @@
 import './App.css'
 import Header from './components/Header'
-
+import { useState } from 'react';
 function App() {
-  const data='Jabeel shahis'
+  const [count,setCount]=useState(0)
+  const addCount=()=>{
+    setCount(count+1)
+    console.log(count);
+  }
   return (
-    <div>
-      <Header data={data}/>
-      <p>{data}</p>
-
+    <div className='App'>
+      <button onClilck={addCount}>Add</button>
+      <h1>Counter : {count}</h1>
     </div>
-    
   );
 }
 
