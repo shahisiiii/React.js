8544be2c86a8566691a184eccd4b68da0846a1f9
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-12-10 12:40:45 +0530

    props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,36 @@
-import './App.css'
-import Header from './components/Header'
+
 import { useState } from 'react';
+import Employee from './Employee';
+
 function App() {
   const [count,setCount]=useState(0)
   const addCount=()=>{
     setCount(count+1)
-    console.log(count);
+    
   }
+  let emp=[{name:'shahis',age:22,},
+  {name:'rahul',age:25,},
+  {name:'manoj',age:14,},
+  {name:'sanoj',age:25,},
+  {name:'ram',age:15,},
+  {name:'dolu',age:28,},
+  {name:'bolu',age:21,},
+  {name:'shahul',age:22,},
+  {name:'sam',age:19,},
+  {name:'mikhil',age:24,},
+  {name:'anu',age:22,},
+  {name:'seetha',age:18,},
+]
   return (
     <div className='App'>
-      <button onClilck={addCount}>Add</button>
-      <h1>Counter : {count}</h1>
+      <button onClick={addCount}>Add</button>
+        {
+          emp.map((obj,index)=> (
+              <Employee key={index} {...obj} />
+            )
+          )
+        }
+    
     </div>
   );
 }
